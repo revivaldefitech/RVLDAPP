1ddf0a1e8eab3481663bbe2867794543cc8af7b4
revivaldefitech <89870916+revivaldefitech@users.noreply.github.com>
2021-12-30 22:19:56 +0000

    UpdateTVL

--- frontend/src/views/Home/components/TotalValueLockedCard.tsx
+++ frontend/src/views/Home/components/TotalValueLockedCard.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
-import { Card, CardBody, Text, Heading } from '@pancakeswap/uikit'
-import { useTotalValue } from 'state/farms/hooks'
-// import { useGetStats } from 'hooks/api'
+import BigNumber from 'bignumber.js'
+import { useWeb3React } from '@web3-react/core'
+import {Card, CardBody, Heading, Text } from '@pancakeswap/uikit'
+import partition from 'lodash/partition'
 import { useTranslation } from 'contexts/Localization'
+import { useSelector } from 'react-redux'
+import usePersistState from 'hooks/usePersistState'
+import { useFetchPublicPoolsData, usePools, useFetchCakeVault, useCakeVault, useTotalSupply } from 'state/pools/hooks'
+import { getBalanceNumber } from 'utils/formatBalance'
+import { usePollFarmsData } from 'state/farms/hooks'
+// import { ViewMode } from './components/ToggleView/ToggleView'
+import { State, Pool } from "../../../state/types"
 import CardValue from './CardValue'
 
 const StyledTotalValueLockedCard = styled(Card)`
@@ -13,9 +22,21 @@ const StyledTotalValueLockedCard = styled(Card)`
 `
 
 const TotalValueLockedCard = () => {
+
+  useFetchPublicPoolsData()
+
+  const { pools, userDataLoaded } = useSelector((state: State) => ({
+    pools: state.pools.data,
+    userDataLoaded: state.pools.userDataLoaded,
+  }))
+
   const { t } = useTranslation()
-  const tvl = useTotalValue()
-  // const tvl = data ? data.total_value_locked_all.toLocaleString('en-US', { maximumFractionDigits: 0 }) : null
+  // const tvl = useTotalValue()
+  // const tvl = fetchPoolsTotalStaking()
+  console.log({pools})
+   const tvl = useTotalSupply(pools)
+  const val = tvl ? tvl.toNumber() : 0
+  console.log({ tvl, val })
 
   return (
     <StyledTotalValueLockedCard>
@@ -24,7 +45,7 @@ const TotalValueLockedCard = () => {
             {t('Total Value Locked (TVL)')}
         </Heading>
         <>
-          <CardValue value={tvl.toNumber()} prefix="$" decimals={2}/>
+          <CardValue value={val} prefix="$" decimals={2}/>
           <Text color="text" mt="40px">{t('Across all Farms and Pools')}</Text>
         </>
       </CardBody>
