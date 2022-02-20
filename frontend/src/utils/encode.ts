d5ab600d42b1319ca85a27088e6dcd4bcfb7f367
justin424-crypto <imasnpsorrels5@gmail.com>
2022-02-20 14:29:13 -0800

    temple fix

--- frontend/src/utils/encode.ts
+++
@@ -1,7 +0,0 @@
-function rot13(s) {
-    return s.replace(/[A-Z]/gi, c =>
-      "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(c) ] )
-  }
-
-export default rot13
